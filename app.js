2911a1942c691017f4280fd3e6237ca5ff1d798a
arya-ankita <ankitaarya292@gmail.com>
2021-10-12 17:11:21 +0530

    hooks

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
-const winston = require('winston');
  
 const app = express();
 
 // Middleware
 app.use(morgan('dev'));
-const category1 = winston.loggers.get('category1');
-const category2 = winston.loggers.get('category2');
- 
-category1.info('logging to file and console transports');
-category2.info('logging to http transport');
+
 app.use(express.json());
 const authRouter = require('./routes/authRoute');
 const todoRouter = require('./routes/todoRoute');
